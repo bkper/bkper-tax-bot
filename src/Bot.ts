35959754e98a962564601afaa8c3ba046b0a3130
mael <mael@bkper.com>
2020-10-24 09:03:46 -0300

    Adding onTransactionUpdated trigger

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,23 +1,33 @@
 BkperApp.setApiKey(PropertiesService.getScriptProperties().getProperty('BOT_KEY'));
 
 /**
- * Trigger called upon transaction posted
+ * Trigger called upon transaction posted. See bkper.yaml for configured triggers.
  */
 function onTransactionPosted(event: bkper.Event) {
   return new EventHandlerTransactionPosted().handleEvent(event);
 }
 
 /**
- * Trigger called upon transaction deleted
+ * Trigger called upon transaction deleted. See bkper.yaml for configured triggers.
  */
 function onTransactionDeleted(event: bkper.Event) {
   return new EventHandlerTransactionDeleted().handleEvent(event);
 }
 
 /**
- * Trigger called upon transaction posted
+ * Trigger called upon transaction restored. See bkper.yaml for configured triggers.
  */
 function onTransactionRestored(event: bkper.Event) {
   //Call same POSTED event handler to repost tax transactions 
+  return new EventHandlerTransactionPosted().handleEvent(event);
+}
+
+/**
+ * Trigger called upon transaction updated. See bkper.yaml for configured triggers.
+ */
+function onTransactionUpdated(event: bkper.Event) {
+  //First call DELETE handler to delete already created tax transactions.
+  new EventHandlerTransactionDeleted().handleEvent(event);
+  //Then call same POSTED event handler to repost tax transactions  
   return new EventHandlerTransactionPosted().handleEvent(event);
 }
