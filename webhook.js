361466d7d362ef177a7864dbe7045197fbff185d
mael <mael@bkper.com>
2021-01-27 09:09:33 -0300

    Make webhook.js run in development NODE_ENV

--- webhook.js
+++ webhook.js
@@ -1,22 +1,34 @@
 const localtunnel = require('localtunnel');
 const Bkper = require('bkper').Bkper;
+
+//Ensure env at right location
 require('dotenv').config();
+
+process.env.NODE_ENV='development';
 
 const app = Bkper.setApiKey(process.env.BKPER_API_KEY);
 
-(async () => {
-  const tunnel = await localtunnel({ port: 3001 });
 
-  await app.setWebhookUrlDev(tunnel.url).patch()
-  console.log(`Listening at ${tunnel.url}`);
-  tunnel.on('close', async () => {
-    await exit()
-  });
+(async () => {
+  try {
+    const tunnel = await localtunnel({ port: 3003 });
+    await app.setWebhookUrlDev(tunnel.url).patch()
+    console.log(`Listening at ${tunnel.url}`);
+    tunnel.on('close', async () => {
+      await exit()
+    });    
+  } catch (err) {
+    console.log(err)
+  }  
 })();
 
 async function exit() {
-  await app.setWebhookUrlDev(null).patch();
-  console.log(' \nRemoved webhook.')
+  try {
+    await app.setWebhookUrlDev(null).patch();
+    console.log(' \nRemoved webhook.')
+  } catch (err) {
+    console.log(err)
+  }
   process.exit();
 }
 
