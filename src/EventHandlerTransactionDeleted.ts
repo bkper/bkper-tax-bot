b91d5bd123a51d59ef7d1b3c16ec2e705862cefe
maelcaldas <mael@bkper.com>
2025-11-18 22:14:54 -0300

    refactor: improve indentation and use trash() instead of remove()
    - Fix import statement spacing
    - Improve class method indentation consistency
    - Replace deprecated remove() with trash() method
    - Uncheck transaction before trashing

--- src/EventHandlerTransactionDeleted.ts
+++ src/EventHandlerTransactionDeleted.ts
@@ -1,97 +1,97 @@
 import { Account, Book, Group } from "bkper-js";
-import {TAX_EXCLUDED_LEGACY_PROP, TAX_EXCLUDED_RATE_PROP, TAX_INCLUDED_LEGACY_PROP, TAX_INCLUDED_RATE_PROP, TAX_RATE_LEGACY_PROP } from "./constants.js";
+import { TAX_EXCLUDED_LEGACY_PROP, TAX_EXCLUDED_RATE_PROP, TAX_INCLUDED_LEGACY_PROP, TAX_INCLUDED_RATE_PROP, TAX_RATE_LEGACY_PROP } from "./constants.js";
 import EventHandler from "./EventHandler.js";
 
 export default class EventHandlerTransactionDeleted extends EventHandler {
 
-  protected async processTransaction(book: Book, transaction: bkper.Transaction, event: bkper.Event): Promise<string[] | string | boolean> {
-    var creditAccount = transaction.creditAccount;
-    var debitAccount = transaction.debitAccount;
+    protected async processTransaction(book: Book, transaction: bkper.Transaction, event: bkper.Event): Promise<string[] | string | boolean> {
+        var creditAccount = transaction.creditAccount;
+        var debitAccount = transaction.debitAccount;
 
-    let transactionsIds: string[] = [];
+        let transactionsIds: string[] = [];
 
-    transactionsIds = transactionsIds.concat(await this.getTaxTransactionsIds(book, creditAccount, transaction));
-    transactionsIds = transactionsIds.concat(await this.getTaxTransactionsIds(book, debitAccount, transaction));
+        transactionsIds = transactionsIds.concat(await this.getTaxTransactionsIds(book, creditAccount, transaction));
+        transactionsIds = transactionsIds.concat(await this.getTaxTransactionsIds(book, debitAccount, transaction));
 
-    if (event.data.previousAttributes) {
-      let oldCreditAccountId = event.data.previousAttributes['creditAccId'];
-      if (oldCreditAccountId && oldCreditAccountId != transaction.creditAccount.id) {
-        let oldCreditAccount = await book.getAccount(oldCreditAccountId);
-        transactionsIds = transactionsIds.concat(await this.getTaxTransactionsIds(book, oldCreditAccount.json(), transaction));
-      }
+        if (event.data.previousAttributes) {
+            let oldCreditAccountId = event.data.previousAttributes['creditAccId'];
+            if (oldCreditAccountId && oldCreditAccountId != transaction.creditAccount.id) {
+                let oldCreditAccount = await book.getAccount(oldCreditAccountId);
+                transactionsIds = transactionsIds.concat(await this.getTaxTransactionsIds(book, oldCreditAccount.json(), transaction));
+            }
 
-      let oldDebitAccountId = event.data.previousAttributes['debitAccId'];
-      if (oldDebitAccountId && oldDebitAccountId != transaction.debitAccount.id) {
-        let oldDebitAccount = await book.getAccount(oldDebitAccountId);
-        transactionsIds = transactionsIds.concat(await this.getTaxTransactionsIds(book, oldDebitAccount.json(), transaction));
-      }
-    }
-
-
-    if (transactionsIds.length == 0) {
-      return false;
-    }
-
-    let deletedRecords: string[] = [];
-    for (const id of transactionsIds) {
-      let tx = (await book.listTransactions(`remoteId:${id}`)).getFirst();
-      if (tx) {
-        if (tx.isChecked()) {
-          tx = await tx.uncheck();
+            let oldDebitAccountId = event.data.previousAttributes['debitAccId'];
+            if (oldDebitAccountId && oldDebitAccountId != transaction.debitAccount.id) {
+                let oldDebitAccount = await book.getAccount(oldDebitAccountId);
+                transactionsIds = transactionsIds.concat(await this.getTaxTransactionsIds(book, oldDebitAccount.json(), transaction));
+            }
         }
-        tx = await tx.remove();
-        deletedRecords.push(`DELETED: ${tx.getDateFormatted()} ${book.formatValue(tx.getAmount())} ${tx.getDescription()}`)
-      }
+
+
+        if (transactionsIds.length == 0) {
+            return false;
+        }
+
+        let deletedRecords: string[] = [];
+        for (const id of transactionsIds) {
+            let tx = (await book.listTransactions(`remoteId:${id}`)).getFirst();
+            if (tx) {
+                if (tx.isChecked()) {
+                    tx = await tx.uncheck();
+                }
+                tx = await tx.trash();
+                deletedRecords.push(`DELETED: ${tx.getDateFormatted()} ${book.formatValue(tx.getAmount())} ${tx.getDescription()}`)
+            }
+        }
+
+        if (deletedRecords.length != 0) {
+            return deletedRecords;
+        } else {
+            return false;
+        }
     }
 
-    if (deletedRecords.length != 0) {
-      return deletedRecords;
-    } else {
-      return false;
-    }
-  }
+    private async getTaxTransactionsIds(book: Book, account: bkper.Account, transaction: bkper.Transaction): Promise<string[]> {
 
-  private async getTaxTransactionsIds(book: Book, account: bkper.Account, transaction: bkper.Transaction): Promise<string[]> {
+        let transactionsIds: string[] = [];
 
-    let transactionsIds: string[] = [];
+        this.addTaxTransactions(book, account, transaction, transactionsIds);
 
-    this.addTaxTransactions(book, account, transaction, transactionsIds);
+        let groups = account.groups;
+        if (groups != null) {
+            for (var group of groups) {
+                this.addTaxTransactions(book, group, transaction, transactionsIds);
+            }
+        }
 
-    let groups = account.groups;
-    if (groups != null) {
-      for (var group of groups) {
-        this.addTaxTransactions(book, group, transaction, transactionsIds);
-      }
+        return transactionsIds;
     }
 
-    return transactionsIds;
-  }
-
-  private addTaxTransactions(book: Book, accountOrGroup: bkper.Account|bkper.Group, transaction: bkper.Transaction, txIds: string[]) {
-    let taxTags = [TAX_RATE_LEGACY_PROP, TAX_INCLUDED_RATE_PROP, TAX_INCLUDED_LEGACY_PROP, TAX_EXCLUDED_RATE_PROP, TAX_EXCLUDED_LEGACY_PROP];
-    for (const taxTag of taxTags) {
-      let taxTxId = this.getTaxTransactionId(book, accountOrGroup, transaction, taxTag);
-      if (taxTxId != null) {
-        txIds.push(taxTxId);
-      }
-    }
-  }
-
-  private getTaxTransactionId(book: Book, accountOrGroup: bkper.Account | bkper.Group, transaction: bkper.Transaction, taxProperty: string,): string {
-
-    if (!accountOrGroup.properties) {
-      return null;
+    private addTaxTransactions(book: Book, accountOrGroup: bkper.Account | bkper.Group, transaction: bkper.Transaction, txIds: string[]) {
+        let taxTags = [TAX_RATE_LEGACY_PROP, TAX_INCLUDED_RATE_PROP, TAX_INCLUDED_LEGACY_PROP, TAX_EXCLUDED_RATE_PROP, TAX_EXCLUDED_LEGACY_PROP];
+        for (const taxTag of taxTags) {
+            let taxTxId = this.getTaxTransactionId(book, accountOrGroup, transaction, taxTag);
+            if (taxTxId != null) {
+                txIds.push(taxTxId);
+            }
+        }
     }
 
-    let taxPropertyValue = accountOrGroup.properties[taxProperty];
+    private getTaxTransactionId(book: Book, accountOrGroup: bkper.Account | bkper.Group, transaction: bkper.Transaction, taxProperty: string,): string {
 
-    if (taxPropertyValue == null || taxPropertyValue.trim() == '') {
-      return null;
+        if (!accountOrGroup.properties) {
+            return null;
+        }
+
+        let taxPropertyValue = accountOrGroup.properties[taxProperty];
+
+        if (taxPropertyValue == null || taxPropertyValue.trim() == '') {
+            return null;
+        }
+
+        let taxTag = taxProperty == TAX_RATE_LEGACY_PROP ? 'tax' : taxProperty;
+
+        return `${super.getId(taxTag, transaction, accountOrGroup)}`
     }
-
-    let taxTag = taxProperty == TAX_RATE_LEGACY_PROP ? 'tax' : taxProperty;
-
-    return `${super.getId(taxTag, transaction, accountOrGroup)}`
-  }
 
 }
